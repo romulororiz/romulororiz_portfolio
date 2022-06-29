0909245e5da9c336cbee95db1ddb4c7a12037579
Romulo Roriz <romulonmroriz@gmail.com>
2022-06-29 15:45:13 +0200

    Setting up Sass

--- components/Layout.js
+++ components/Layout.js
@@ -9,10 +9,6 @@ export const Layout = ({ title, description, keywords, children }) => {
 				<title>{title}</title>
 				<meta name='description' content={description} />
 				<meta name='keywords' content={keywords} />
-				<meta
-					name='google-site-verification'
-					content='AIHkU7uwE26LtE6yMOkrFd9WMqwIGT9XLT1suf2Gfu8'
-				/>
 			</Head>
 			<Header />
 			<div>{children}</div>
