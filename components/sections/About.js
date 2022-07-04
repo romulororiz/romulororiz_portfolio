0f328cc222bf889e28ec10ae3f3af7f0e75f32a3
Romulo Roriz <romulonmroriz@gmail.com>
2022-07-04 17:19:58 +0200

    Setting up embla carousel for about section

--- components/sections/About.js
+++ components/sections/About.js
@@ -1,6 +1,25 @@
+import { useEffect } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import emblaCarouselReact from 'embla-carousel-react';
 import styles from '@/styles/About.module.scss';
 
 const About = () => {
+	const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
+
+	useEffect(() => {
+		if (emblaApi) {
+		}
+	}, [emblaApi]);
+
+	// Grab wrapper nodes
+	const rootNode = document.querySelector(styles.carousel);
+	const viewportNode = document.querySelector(styles.carousel_viewport);
+	const embla = emblaCarouselReact(rootNode);
+
+	// Grab button nodes
+	const prevButtonNode = rootNode.querySelector(styles.embla_prev);
+	const nextButtonNode = rootNode.querySelector(styles.embla_next);
+
 	return (
 		<div id='about' className={styles.about}>
 			<h1>About Me</h1>
@@ -12,8 +31,16 @@ const About = () => {
 					quaerat magni vero iusto nesciunt iste voluptate exercitationem
 					blanditiis facere officiis! Aspernatur eius facere temporibus nemo.
 				</p>
-				<div className={styles.carousel}>
-					<p>carousel</p>
+				<div className={styles.carousel} ref={emblaRef}>
+					<div className={styles.carousel_viewport}>
+						<div className={styles.carousel_container}>
+							<div className={styles.carousel_slide}>Slide 1</div>
+							<div className={styles.carousel_slide}>Slide 2</div>
+							<div className={styles.carousel_slide}>Slide 3</div>
+						</div>
+					</div>
+					<button className={styles.embla_prev}>Prev</button>
+					<button className={styles.embla_next}>Next</button>
 				</div>
 			</div>
 		</div>
