cfff78c4500aa64515f39d2a05d4c16aa524834e
Romulo Roriz <romulonmroriz@gmail.com>
2022-07-07 08:59:00 +0200

    fix Heading bug on iOS

--- components/sections/Projects.js
+++ components/sections/Projects.js
@@ -8,7 +8,21 @@ const Projects = () => {
 				<Icon name='caretR' />
 				Projects
 			</h1>
-			<div></div>
+			<div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+					dolorem incidunt quam nesciunt minima nisi quo qui at, maxime et, est
+					doloremque eius inventore! Non dolore, cumque sint architecto error
+					veritatis, impedit facere nesciunt amet labore, similique optio! Quas
+					voluptatibus minima dolor magnam sequi voluptates accusantium
+					voluptatem, laborum, sint velit commodi sunt ratione harum voluptas
+					neque ut delectus libero maiores quos recusandae non aliquid esse!
+					Necessitatibus earum, quo voluptate veritatis magnam odio obcaecati
+					repellat omnis natus non minus totam facere inventore pariatur
+					distinctio quidem sequi beatae vero reiciendis deleniti ullam. Nostrum
+					minus accusamus tempore nihil consectetur. Magnam rem numquam id?
+				</p>
+			</div>
 		</div>
 	);
 };
