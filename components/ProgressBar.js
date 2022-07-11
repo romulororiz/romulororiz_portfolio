f1ffd476a626785ae8eb4a97a282839f25a06b80
Romulo Roriz <romulonmroriz@gmail.com>
2022-07-11 21:22:37 +0200

    Add Animation progress bar

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,12 +1,16 @@
 import styles from '@/styles/ProgressBar.module.scss';
+import { motion } from 'framer-motion';
 
 const ProgressBar = ({ percentage }) => {
 	return (
 		<div className={styles.progressBarWrapper}>
-			<span
+			<motion.span
+				initial={{ width: 0 }}
+				whileInView={{ width: `${percentage}` }}
+				viewport={{ once: 'true', amount: 'some' }}
+				transition={{ duration: 2 }}
 				className={styles.progressBar}
-				style={{ width: `${percentage}` }}
-			></span>
+			></motion.span>
 		</div>
 	);
 };
